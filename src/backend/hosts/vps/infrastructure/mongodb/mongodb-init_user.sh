6586e984f47e52f87bf4407855713cb007ce3f9b
Andrey Pestunov <masterlifting@gmail.com>
2024-01-06 14:23:54 +0100

    refactor: folder names

--- src/backend/hosts/vps/infrastructure/mongodb/mongodb-init_user.sh
+++ src/backend/hosts/vps/infrastructure/mongodb/mongodb-init_user.sh
@@ -2,8 +2,11 @@
 echo "Waiting for user to be created..."
 sleep 2
 
+userName=process.env.MONGODBCONNECTION_USERNAME
+userPassword=process.env.MONGODBCONNECTION_PASSWORD
+
 mongosh admin --eval 'db.createUser({
-  user: "admin",
-  pwd: "adminPassword",
+  user: "$userName",
+  pwd: "$userPassword",
   roles: [{ role: "root", db: "admin" }]
 });'
