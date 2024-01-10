2128e14af3c142847093bed226ef4f20eea622e7
Andrey Pestunov <masterlifting@gmail.com>
2024-01-10 02:52:46 +0100

    fix: docker-compose

--- .docker-compose/infrastructure/mongodb/mongodb-init.sh
+++
@@ -1,7 +0,0 @@
-echo "Waiting for keyfile to be created..."
-sleep 2
-
-chmod 400 /opt/keyfiles/mongodb-keyfile
-mongod --logpath /var/log/mongodb.log --bind_ip_all --replSet rs0 --keyFile /opt/keyfiles/mongodb-keyfile
-
-echo "Keyfile created successfully"
