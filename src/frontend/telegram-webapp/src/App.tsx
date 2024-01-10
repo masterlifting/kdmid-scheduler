1b198d541d9a1433322760a00ab406c27b8f582e
Andrey Pestunov <masterlifting@gmail.com>
2024-01-10 04:11:17 +0100

    fix: web routing

--- src/frontend/telegram-webapp/src/App.tsx
+++ src/frontend/telegram-webapp/src/App.tsx
@@ -7,7 +7,7 @@ import { KdmidIdPage } from './pages/KdmidIdPage';
 export const App = () => (
   <div className='container mx-auto max-w-2xl pt-5'>
     <Routes>
-      <Route path='/kdmidweb/kdmidId' element={<KdmidIdPage />} />
+      <Route path='/kdmidId' element={<KdmidIdPage />} />
       <Route path='*' element={<h1>This page does not exist</h1>} />
     </Routes>
   </div>
