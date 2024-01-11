f58adabf4fe858fb18de321fe1e65ea1ede098f1
Andrey Pestunov <masterlifting@gmail.com>
2024-01-11 11:01:07 +0100

    refactor: test ci-cd pipline

--- .github/workflows/deploy-full.sh
+++ .github/workflows/deploy-full.sh
@@ -12,4 +12,6 @@ chmod 600 .docker-compose/infrastructure/traefik/traefik.yml
 chmod 600 .docker-compose/infrastructure/traefik/letsencrypt/acme.json
 chown root:root .docker-compose/infrastructure/traefik/letsencrypt/acme.json
 
-docker-compose -p kdmid -f .docker-compose/docker-compose.vps.yml up -d --build --force-recreate --remove-orphans
+docker-compose -p kdmid -f .docker-compose/docker-compose.vps.yml down
+docker-compose -p kdmid -f .docker-compose/docker-compose.vps.yml up -d --build
+docker system prune -af --volumes
