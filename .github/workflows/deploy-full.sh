9204dbcc4ca2071de4905d9b174b3e6ab4d57e74
Andrey Pestunov <masterlifting@gmail.com>
2024-01-11 07:32:33 +0100

    feat: add ci-cd

---
+++ .github/workflows/deploy-full.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -e
+
+cd ../../
+
+git stash
+git pull origin main
+
+chmod 600 .docker-compose/infrastructure/mongodb/mongodb-keyfile
+chmod 600 .docker-compose/infrastructure/traefik/traefik.yml
+chmod 600 .docker-compose/infrastructure/traefik/letsencrypt/acme.json
+chown root:root .docker-compose/infrastructure/traefik/letsencrypt/acme.json
+
+docker-compose -p kdmid -f .docker-compose/docker-compose.vps.yml up -d --build --force-recreate --remove-orphans
