3812759bfd85bc6ded28c4130caa45f1fb403a8e
Andrey Pestunov <masterlifting@gmail.com>
2024-01-12 09:50:16 +0100

    refactror: improve deploy

--- .github/workflows/deploy-full.sh
+++ .github/workflows/deploy-full.sh
@@ -7,10 +7,7 @@ cd /usr/src/kdmid-scheduler
 git stash
 git pull origin main --recurse-submodules
 
-chmod 600 .docker-compose/infrastructure/mongodb/mongodb-keyfile
 chmod 600 .docker-compose/infrastructure/traefik/traefik.yml
-chmod 600 .docker-compose/infrastructure/traefik/letsencrypt/acme.json
-chown root:root .docker-compose/infrastructure/traefik/letsencrypt/acme.json
 
 docker-compose -p kdmid -f .docker-compose/docker-compose.vps.yml down
 docker-compose -p kdmid -f .docker-compose/docker-compose.vps.yml up -d --build
