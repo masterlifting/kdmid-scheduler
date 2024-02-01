237b6bac2dd28649d1d72c204bf75249b83f4589
Andrey Pestunov <masterlifting@gmail.com>
2024-02-01 22:46:39 +0100

    refactor: build mongodb script

--- .docker-compose/infrastructure/mongodb/kdmid.mongodb.js
+++ .docker-compose/infrastructure/mongodb/kdmid.mongodb.js
@@ -4,30 +4,30 @@ use('kdmid');
 
 var kdmidValues = db.KdmidBotCommands.find({
   $where: function () {
-    var kdmidIdStr = this.Command.Parameters['KdmidScheduler.Abstractions.Models.Core.v1.KdmidId'];
-    if (kdmidIdStr) {
-      var kdmidId = JSON.parse(kdmidIdStr);
-      return kdmidId.id === '72095';
+    var cityStr = this.Command.Parameters['v1.City'];
+    if (cityStr) {
+      var city = JSON.parse(cityStr);
+      return city.Code === 'Ljubljana';
     }
     return false;
   },
 });
 
-kdmidValues.forEach(function (doc) {
-  var parameters = doc.Command.Parameters;
-  var kdmidId = JSON.parse(parameters['KdmidScheduler.Abstractions.Models.Core.v1.KdmidId']);
+// kdmidValues.forEach(function (doc) {
+//   var parameters = doc.Command.Parameters;
+//   var kdmidId = JSON.parse(parameters['v1.KdmidId']);
 
-  kdmidId.cd = '68672fa8';
+//   kdmidId.cd = '68672fa8';
 
-  parameters['KdmidScheduler.Abstractions.Models.Core.v1.KdmidId'] = JSON.stringify(kdmidId);
+//   parameters['v1.KdmidId'] = JSON.stringify(kdmidId);
 
-  var updateResult = db.KdmidBotCommands.updateOne({ _id: doc._id }, { $set: { 'Command.Parameters': parameters } });
+//   var updateResult = db.KdmidBotCommands.updateOne({ _id: doc._id }, { $set: { 'Command.Parameters': parameters } });
 
-  if (updateResult.matchedCount === 0) {
-    print('No document matched');
-  } else if (updateResult.modifiedCount === 0) {
-    print('Document matched but not modified');
-  } else {
-    print('Document updated');
-  }
-});
+//   if (updateResult.matchedCount === 0) {
+//     print('No document matched');
+//   } else if (updateResult.modifiedCount === 0) {
+//     print('Document matched but not modified');
+//   } else {
+//     print('Document updated');
+//   }
+// });
