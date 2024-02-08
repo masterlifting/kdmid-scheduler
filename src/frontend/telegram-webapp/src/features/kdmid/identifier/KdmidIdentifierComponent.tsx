3a14ee4a8df8fb5e2954c878cb1e01ba40b3b21d
Andrey Pestunov <masterlifting@gmail.com>
2024-02-08 10:58:09 +0100

    refactor: paginated commands and update button event handler

--- src/frontend/telegram-webapp/src/features/kdmid/identifier/KdmidIdentifierComponent.tsx
+++ src/frontend/telegram-webapp/src/features/kdmid/identifier/KdmidIdentifierComponent.tsx
@@ -16,7 +16,7 @@ export const KdmidIdentifier = ({ chatId, cityCode }: IKdmidIdentifierProps) => 
     city,
     commands,
     commandsTotalCount,
-    onAddNewCommand,
+    onNewCommand,
     onSetCommand,
     onRemoveCommand,
     onChangeKdmidId,
@@ -30,20 +30,20 @@ export const KdmidIdentifier = ({ chatId, cityCode }: IKdmidIdentifierProps) => 
     <div className='w-80 absolute rounded-md left-1/2 top-1/3 transform -translate-x-1/2 -translate-y-1/3'>
       <div className='grid grid-cols-[1fr,auto] gap-1 items-center'>
         <span className='text-3xl font-bold text-left'>{city?.name ?? 'Loading...'}</span>
-        <button type='button' className={ButtonClass.Success} onClick={onAddNewCommand}>
+        <button type='button' className={ButtonClass.Success} onClick={onNewCommand}>
           New
         </button>
       </div>
-      <div className=' h-80 overflow-y-auto'>
+      <div>
         {commands.map(x => (
           <form key={x.key} className='mt-3'>
             <div className='flex flex-col items-center'>
               <input
                 className={InputClass.Text}
                 name='id'
-                title='id'
+                title='id from your kdmid email link'
                 type='text'
-                placeholder='id'
+                placeholder='id from your link'
                 autoComplete='id'
                 value={x.command.identifier.id}
                 onChange={e => onChangeKdmidId(e, x.key)}
@@ -51,9 +51,9 @@ export const KdmidIdentifier = ({ chatId, cityCode }: IKdmidIdentifierProps) => 
               <input
                 className={InputClass.Text}
                 name='cd'
-                title='cd'
+                title='cd from your kdmid email link'
                 type='text'
-                placeholder='cd'
+                placeholder='cd from your link'
                 autoComplete='cd'
                 value={x.command.identifier.cd}
                 onChange={e => onChangeKdmidCd(e, x.key)}
@@ -61,9 +61,9 @@ export const KdmidIdentifier = ({ chatId, cityCode }: IKdmidIdentifierProps) => 
               <input
                 className={InputClass.Text}
                 name='ems'
-                title='ems'
+                title='ems from your kdmid email link. Optional'
                 type='text'
-                placeholder='ems'
+                placeholder='ems from your link'
                 autoComplete='ems'
                 value={x.command.identifier.ems}
                 onChange={e => onChangeKdmidEms(e, x.key)}
