9a2668b4b3d9a3b77e0a764a202e3db8d24d1c93
Andrey Pestunov <masterlifting@gmail.com>
2024-02-09 08:32:22 +0100

    refactor: Update input styles to use black text

--- src/frontend/telegram-webapp/src/styles/input.ts
+++ src/frontend/telegram-webapp/src/styles/input.ts
@@ -1,6 +1,6 @@
 /** @format */
 
 export enum InputClass {
-  Text = 'h-10 w-full my-1 py-1 px-2 border border-gray-300 rounded-md focus:ring-2 focus:ring-blue-300 ',
-  Select = 'w-full my-1 py-1 px-2 border border-gray-300 rounded-md focus:ring-2 focus:ring-blue-300 cursor-pointer ',
+  Text = 'h-10 w-full my-1 py-1 px-2 border border-gray-300 rounded-md focus:ring-2 focus:ring-blue-300 text-black ',
+  Select = 'w-full my-1 py-1 px-2 border border-gray-300 rounded-md focus:ring-2 focus:ring-blue-300 cursor-pointer  text-black ',
 }
